585f2216ecc4829f8eb3730bea4367c1978e65e3
David <19208571+telekpsi@users.noreply.github.com>
2022-10-21 12:24:08 -0600

    Update server.js

--- server.js
+++ server.js
@@ -1,42 +1,9 @@
 const app = require("./backend/app");
-const debug = require("debug")("node-angular");
 const http = require("http");
-
-const normalizePort = val => {
-  var port = parseInt(val, 10);
-
-  if (isNaN(port)) {
-    // named pipe
-    return val;
-  }
-
-  if (port >= 0) {
-    // port number
-    return port;
-  }
-
-  return false;
-};
-
-const onError = error => {
-  if (error.syscall !== "listen") {
-    throw error;
-  }
-  const bind = typeof port === "string" ? "pipe " + port : "port " + port;
-  switch (error.code) {
-    case "EACCES":
-      console.error(bind + " requires elevated privileges");
-      process.exit(1);
-      break;
-    case "EADDRINUSE":
-      console.error(bind + " is already in use");
-      process.exit(1);
-      break;
-    default:
-      throw error;
-  }
-};
-
+app.use(express.static('dist/PROJECT-NAME'));
+app.get('/', function (req, res,next) {
+res.redirect('/');
+});
 const onListening = () => {
   const addr = server.address();
   const bind = typeof port === "string" ? "pipe " + port : "port " + port;
