2c055d3228829850ac3537e09cc9ccc7e94b2832
telekpsi <19208571+telekpsi@users.noreply.github.com>
2022-10-21 15:00:36 -0400

    server might be right?

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = require("./backend/app");
 const debug = require("debug")("node-angular");
 const http = require("http");
-app.use(express.static('dist/PROJECT-NAME'));
+app.use(express.static('dist/meancourse'));
 app.get('/', function (req, res,next) {
 res.redirect('/');
 });
